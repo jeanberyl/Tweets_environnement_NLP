4293ab6add7fab6b81c38ed296e32031d6b25d18
jeanberyl <jbe14z@gmail.com>
2020-04-28 16:58:56 +0200

    api call dataframe concat and write csv file

--- nlptweets/twitter.py
+++ nlptweets/twitter.py
@@ -1,5 +1,6 @@
 import json
 import os
+import csv
 
 import numpy as np
 import pandas as pd
@@ -17,129 +18,152 @@ try:
 except Exception as e:
     raise RuntimeError('Could not read keys from credentials file. {a} {b}'.format(a=repr(e), b=fpath))
 
+
+try:
+    fpath = os.path.join(os.path.dirname(__file__), os.pardir, 'resources', 'liste_twittos_energie.csv')
+    # with open(fpath) as liste_twittos_energie:
+    #     list_twittos = csv.reader(liste_twittos_energie, delimiter=',')
+    list_twittos = pd.read_csv(fpath, sep= ',')
+
+except Exception as e:
+    raise RuntimeError('Could not read csv')
+
+print(list_twittos)
+
 AUTH = tweepy.OAuthHandler(key1, key2)
 AUTH.set_access_token(key3, key4)
 
 TWITTER_API = tweepy.API(AUTH)
 
-def get_home_timeline(include_entities=True):
-    return TWITTER_API.home_timeline(include_entities=include_entities)
+# def get_home_timeline(include_entities=True):
+#     return TWITTER_API.home_timeline(include_entities=include_entities)
 
-def get_user_timeline(username, include_entities=True, tweet_mode='extended'):
-    return TWITTER_API.user_timeline(username, include_entities=include_entities, tweet_mode=tweet_mode)
-
-
-print(get_home_timeline())
+# def get_user_timeline(username, include_entities=True, tweet_mode='extended'):
+#     return TWITTER_API.user_timeline(username, include_entities=include_entities, tweet_mode=tweet_mode)
 
 
-
-# # api = tweepy.API(auth)
+# print(get_home_timeline())
 
 
 
-# def extract_tweet_attributes3(tweet_object):
-#     """ 
-#     Function to extract data from tweet object and put into a dataframe 
-#     """
+# api = tweepy.API(auth)
 
-#     tweet_list =[]
+
+
+def extract_tweet_attributes3(tweet_object):
+    """ 
+    Function to extract data from tweet object and put into a dataframe 
+    """
+
+    tweet_list =[]
     
-#     for tweet in tweet_object:
+    for tweet in tweet_object:
         
-#         tweet_id = tweet.id
-#         created_at = tweet.created_at
-#         try: 
-#             full_text = tweet.full_text
-#         except: 
-#             full_text = np.nan
-#         try:
-#             text = tweet.text
-#         except: 
-#             text = np.nan
-#         user = tweet.user.screen_name
-#         entities = tweet.entities
+        tweet_id = tweet.id
+        created_at = tweet.created_at
+        try: 
+            full_text = tweet.full_text
+        except: 
+            full_text = np.nan
+        try:
+            text = tweet.text
+        except: 
+            text = np.nan
+        user = tweet.user.screen_name
+        entities = tweet.entities
         
     
         
-#         tweet_list.append({'tweet_id':tweet_id,  
-#                            'date': created_at,
-#                            'full_text': full_text,
-#                            'text': text, 
-#                            'user':user,
-#                            'entities':entities})
+        tweet_list.append({'tweet_id':tweet_id,  
+                           'date': created_at,
+                           'full_text': full_text,
+                           'text': text, 
+                           'user':user,
+                           'entities':entities})
 
 
 
-#         df = pd.DataFrame(tweet_list, columns=['tweet_id',
-#                                                 'date',
-#                                                 'full_text',
-#                                                 'text',
-#                                                 'user',
-#                                                 'entities'])
-#     return df    
+        df = pd.DataFrame(tweet_list, columns=['tweet_id',
+                                                'date',
+                                                'full_text',
+                                                'text',
+                                                'user',
+                                                'entities'])
+    return df    
 
 
 
 
-# def extract_twittos_max3600(twittos):
-#     """
-#         Function to extract tweets rom twittos list
-#         add dataframes in next line   
-#     """
+def extract_twittos_max3600(twittos):
+    """
+        Function to extract tweets rom twittos list
+        add dataframes in next line   
+    """
     
-#     tweets_data_list = []
+    tweets_data_list = []
     
-#     for i in range(len(twittos)):
+    for i in range(len(twittos)):
     
-#         ID = liste_twittos_energie['id_twitter'][i]
+        ID = list_twittos['id_twitter'][i]
         
-#         multiple_tweets = api.user_timeline(ID, count = 200)
-#         tweets_data_list.append(multiple_tweets)
+        multiple_tweets = TWITTER_API.user_timeline(ID, count = 200)
+        tweets_data_list.append(multiple_tweets)
         
-#         while len(multiple_tweets) == 200:
+        while len(multiple_tweets) == 200:
 
-#             last_tweet_id = multiple_tweets[199].id
+            last_tweet_id = multiple_tweets[199].id
 
-#             multiple_tweets = api.user_timeline(ID, 
-#                                                 count = 200, 
-#                                                 max_id = last_tweet_id)
+            multiple_tweets = TWITTER_API.user_timeline(ID, 
+                                                count = 200, 
+                                                max_id = last_tweet_id)
 
-#             tweets_data_list.append(multiple_tweets)
+            tweets_data_list.append(multiple_tweets)
         
 
-#     return tweets_data_list           
+    return tweets_data_list           
 
 
 
 
 
-# # use function 
-# # rename var
-# data_list = extract_twittos_max3600(liste_twittos_energie)        
+# use function 
+# rename var
+data_list = extract_twittos_max3600(list_twittos)        
 
-# # check len
-# len(data_list)
+# check len
+print("len data_list", len(data_list))
 
 
-# # say what you do and do what you say
-# # function "extract_tweet_attributes2" to make dataframes
-# # all df have same columns names, join on 
+# say what you do and do what you say
+# function "extract_tweet_attributes2" to make dataframes
+# all df have same columns names, join on 
 
-# # rename var
-# df_list = []
+# rename var
+df_list = []
 
-# # extract_tweet_attributes2(data_list[0])
-# for i in range(len(data_list)):
+# extract_tweet_attributes2(data_list[0])
+for i in range(len(data_list)):
     
-#     df = extract_tweet_attributes3(data_list[i])
-#     df_list.append(df)
+    df = extract_tweet_attributes3(data_list[i])
+    df_list.append(df)
 
-# # check len
-# len(df_list)
+# check len
+print("len df_list", len(df_list))
 
-# # check dims of the dataframe
-# table = pd.concat(df_list)
-# table.shape
+# check dims of the dataframe
+table = pd.concat(df_list)
+print("table shape", table.shape)
+
+print(table.head())
 
 
-# # function to de duplicate tweets in order append new tweets to old tweets to grow the tweet dataframe 
+try:
+    fpath = os.path.join(os.path.dirname(__file__), os.pardir, 'resources', 'table_max_april28.csv')
+    table.to_csv(fpath)
+
+except Exception as e:
+    raise RuntimeError('Could not write csv')
+
+
+
+# function to de duplicate tweets in order append new tweets to old tweets to grow the tweet dataframe 
